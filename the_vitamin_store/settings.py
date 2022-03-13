9f31d97f4b5f34d318078da835bea503000fde1a
Sonicbasedrop <ivan.ulysses@hotmail.com>
2022-03-13 19:42:21 +0000

    Removed secret ket and set debug

--- the_vitamin_store/settings.py
+++ the_vitamin_store/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h5b6u7yqz#!3ks3k(o_h7_5)55lqfq-+za((a%qfy-#0ya7wzk'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-vitamin-store.herokuapp.com', 'localhost']
 

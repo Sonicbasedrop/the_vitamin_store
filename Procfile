c19076ca11c2e054f31118430a5549b4415ce3dc
Sonicbasedrop <ivan.ulysses@hotmail.com>
2022-03-13 18:22:48 +0000

    Deployment to Heroku part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn the_vitamin_store.wsgi:application
--- the_vitamin_store/settings.py
+++ the_vitamin_store/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h5b6u7yqz#!3ks3k(o_h7_5)55lqfq-+za((a%qfy-#0ya7wzk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-vitamin-store.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'the_vitamin_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation

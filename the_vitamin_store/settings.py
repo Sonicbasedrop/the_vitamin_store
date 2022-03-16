cfa24cc68742e0eae693d78d129634475177815d
Sonicbasedrop <ivan.ulysses@hotmail.com>
2022-03-16 15:38:49 +0000

    added comment to settings.py

--- the_vitamin_store/settings.py
+++ the_vitamin_store/settings.py
@@ -12,9 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 import dj_database_url
-# from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 

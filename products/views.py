83a78c4ef20c33086f4db83882e97b732f89fe15
Sonicbasedrop <ivan.ulysses@hotmail.com>
2022-03-17 20:59:32 +0000

    Added import Review model

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
-
+from reviews.models import Review
 from .models import Product, Category
 from .forms import ProductForm
 
@@ -64,8 +64,10 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
+    review = Review.objects.filter(product=product)
 
     context = {
+        'review': review,
         'product': product,
     }
 
@@ -86,7 +88,7 @@ def add_product(request):
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to add product. Please ensure the form is valid!.')
     else:
         form = ProductForm()
 
@@ -102,7 +104,7 @@ def add_product(request):
 def edit_product(request, product_id):
     """ Edit a product in the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+        messages.error(request, 'Sorry, only store owners can do that!.')
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
@@ -113,7 +115,7 @@ def edit_product(request, product_id):
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to update product. Please ensure the form is valid!.')
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')

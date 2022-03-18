76aac366fdf30e0d0c47b972dffa9a5484fdcc93
Sonicbasedrop <ivan.ulysses@hotmail.com>
2022-03-18 20:54:03 +0000

    Updated return string in models.py

--- reviews/models.py
+++ reviews/models.py
@@ -16,11 +16,11 @@ class Review(models.Model):
     review_text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        blank=True, null=True, default='')
+    User, on_delete=models.CASCADE,
+    blank=True, null=True, default='')
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE,
         blank=True, null=True, default='')
 
     def __str__(self):
-        return self.title
+        return self.review_text

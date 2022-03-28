85f43d8a2236364b882fba50a4191ecf965ae09d
Sonicbasedrop <ivan.ulysses@hotmail.com>
2022-03-28 13:31:15 +0000

    removed redundant code

--- contact/forms.py
+++ contact/forms.py
@@ -12,16 +12,3 @@ class ContactForm(forms.ModelForm):
         """
         model = Contact
         fields = '__all__'
-
-    # def __init__(self, *args, **kwargs):
-    #     """
-    #     Add placeholders and classes, remove auto-generated
-    #     labels and set autofocus on first field
-    #     """
-    #     super().__init__(*args, **kwargs)
-    #     placeholders = {
-    #         'default_name': 'Name',
-    #         'default_email': 'Email',
-    #         'default_subject': 'Subject',
-    #         'default_message': 'Message',
-    #     }
